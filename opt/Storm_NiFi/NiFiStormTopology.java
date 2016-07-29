78ffc844d0333d02bd6b5e5f35306ba15bc5a1fc
jobinthompu <jobinthompu@gmail.com>
2016-07-28 18:08:40 -0700

    Storm NiFi Site-to-Site Integration

--- opt/Storm_NiFi/NiFiStormTopology.java
+++ opt/Storm_NiFi/NiFiStormTopology.java
@@ -1,23 +1,36 @@
-package wordcount;
+package NiFi;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.AuthorizationException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Tuple;
-import backtype.storm.utils.Utils;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.nifi.remote.client.SiteToSiteClient;
 import org.apache.nifi.remote.client.SiteToSiteClientConfig;
 import org.apache.nifi.storm.NiFiDataPacket;
 import org.apache.nifi.storm.NiFiSpout;
+
 public class NiFiStormTopology {
-public static void main( String[] args ) {
+
+public static void main( String[] args ) throws AlreadyAliveException, InvalidTopologyException, AuthorizationException {
+
+final String Path = args[0];
 // Build a Site-To-Site client config
 SiteToSiteClientConfig clientConfig = new SiteToSiteClient.Builder()
-.url("http://HW12626:8080/nifi")
+.url("http://localhost:8099/nifi")
 .portName("OUT")
 .buildConfig();
 // Build a topology starting with a NiFiSpout
@@ -27,18 +40,37 @@ builder.setSpout("nifi", new NiFiSpout(clientConfig));
 builder.setBolt("print", new BaseBasicBolt() {
 
 private static final long serialVersionUID = 1L;
-@Override
+
 public void execute(Tuple tuple, BasicOutputCollector collector) {
 NiFiDataPacket dp = (NiFiDataPacket) tuple.getValueByField("nifiDataPacket");
+
+//Print Attributes
 System.out.println("Attributes: " + dp.getAttributes());
+
+//Print Content
 System.out.println("Content: " + new String(dp.getContent()));
+//Save it to local
+try {
+	String fileExt = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+	FileUtils.writeStringToFile(new File(Path+fileExt), new String(dp.getContent()));
+	} 
+catch (IOException e) {
+	e.printStackTrace();
 }
-@Override
+
+}
+
 public void declareOutputFields(OutputFieldsDeclarer declarer) {}
 }).shuffleGrouping("nifi");
-// Submit the topology running in local mode
+// Submit the topology running in Cluster mode
 Config conf = new Config();
+StormSubmitter.submitTopology("NiFi-Storm", conf, builder.createTopology());
+
+/*
+Comment above line and uncomment below two lines to run in local mode
 LocalCluster cluster = new LocalCluster();
 cluster.submitTopology("test", conf, builder.createTopology());
+
+*/
 }
 }
